17003e76e92901606babb37f0f178bae0e3b9797
Vasishath Kaushal <vasishath@gmail.com>
2018-06-04 23:48:26 +0530

    sakura: Import config.fs from qcom common
    Signed-off-by: Jyotiraditya <Jyotiraditya182@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -37,6 +37,9 @@ TARGET_USES_64_BIT_BINDER := true
 TARGET_BOOTLOADER_BOARD_NAME := msm8953
 TARGET_NO_BOOTLOADER := true
 
+# Filesystem
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.console=ttyHSL0 androidboot.hardware=qcom msm_rtb.filter=0x237 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 androidboot.bootdevice=7824900.sdhci earlycon=msm_hsl_uart,0x78af000 buildvariant=user
