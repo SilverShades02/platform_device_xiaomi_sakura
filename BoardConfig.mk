1d70b8d64da885924a902d94c4f821ea9a16eabd
Jyotiraditya <Jyotiraditya182@gmail.com>
2018-12-19 22:47:16 +0530

    sakura: Initialise board variables
    Signed-off-by: Jyotiraditya <Jyotiraditya182@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,3 +13,29 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+BOARD_VENDOR := xiaomi
+DEVICE_PATH := device/xiaomi/sakura
+
+# Architecture
+TARGET_ARCH := arm64
+TARGET_ARCH_VARIANT := armv8-a
+TARGET_CPU_ABI := arm64-v8a
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := cortex-a53
+
+TARGET_BOARD_SUFFIX := _64
+TARGET_USES_64_BIT_BINDER := true
+
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := msm8953
+TARGET_NO_BOOTLOADER := true
+
+# Inherit from proprietary files
+-include vendor/xiaomi/sakura/BoardConfigVendor.mk
--- device.mk
+++ device.mk
@@ -13,3 +13,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
+
+# Call the proprietary setup
+$(call inherit-product-if-exists, vendor/xiaomi/sakura/sakura-vendor.mk)
