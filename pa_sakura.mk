27494e0012d488cc3d58f215c7513e21eb70712d
Jyotiraditya <jyotiraditya182@gmail.com>
2019-01-02 10:43:09 +0000

    ParanoidAndroid: Hello Sakura!
    Signed-off-by: Jyotiraditya <jyotiraditya182@gmail.com>

--- pa_sakura.mk
+++ pa_sakura.mk
@@ -1,11 +1,11 @@
 #
-# Copyright (C) 2017 The LineageOS Project
+# Copyright 2018 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -14,12 +14,22 @@
 # limitations under the License.
 #
 
+# Check for the target product.
+ifeq (pa_sakura,$(TARGET_PRODUCT))
+
+# Inherit from our common CAF device tree.
+include device/qcom/common/common.mk
+
+# Inherit the device configuration itself.
 $(call inherit-product, device/xiaomi/sakura/full_sakura.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := sakura
+PRODUCT_NAME := pa_sakura
+PRODUCT_BRAND := Xiaomi
+PRODUCT_MODEL := Redmi 6 Pro
+PRODUCT_MANUFACTURER := Xiaomi
 
-PRODUCT_NAME := lineage_sakura
 BOARD_VENDOR := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
@@ -29,3 +39,8 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 # Set BUILD_FINGERPRINT variable to be picked up by both system and vendor build.prop
 BUILD_FINGERPRINT := xiaomi/sakura/sakura:8.1.0/OPM1.171019.019/V10.0.5.0.ODICNFH:user/release-keys
+
+# Paranoid Android platform
+include vendor/pa/main.mk
+
+endif
